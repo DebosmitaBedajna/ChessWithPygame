a1521d4591fc9b98729266bf892a96ebced715ec
Debosmita Bedajna <bedajnadebosmita@gmail.com>
2024-06-25 17:19:32 +0530

    Pieces add

--- main.py
+++ main.py
@@ -36,6 +36,57 @@ selection=100
 valid_moves=[]
 
 #load in the images for the pieces
+#Black Images
+#Queen
+bq= pygame.image.load('/assets/images/bQ.png')
+bq=pygame.image.scale(bq,(80,80))
+bq_small=pygame.image.scale(bq,(65,65))
+#King
+bk= pygame.image.load('/assets/images/bK.png')
+bk=pygame.image.scale(bk,(80,80))
+bk_small=pygame.image.scale(bk,(65,65))
+#Rook
+br= pygame.image.load('/assets/images/bR.png')
+br=pygame.image.scale(br,(80,80))
+br_small=pygame.image.scale(br,(65,65))
+#Knight
+bn= pygame.image.load('/assets/images/bN.png')
+bn=pygame.image.scale(bn,(80,80))
+bn_small=pygame.image.scale(bn,(65,65))
+#Pawn
+bp= pygame.image.load('/assets/images/bP.png')
+bp=pygame.image.scale(bp,(60,60))
+bp_small=pygame.image.scale(bp,(45,45))
+#Bishop
+bb= pygame.image.load('/assets/images/bB.png')
+bb=pygame.image.scale(bb,(80,80))
+bb_small=pygame.image.scale(bb,(65,65))
+
+#White Images
+#Queen
+wq= pygame.image.load('/assets/images/wQ.png')
+wq=pygame.image.scale(wq,(80,80))
+wq_small=pygame.image.scale(wq,(65,65))
+#King
+wk= pygame.image.load('/assets/images/wK.png')
+wk=pygame.image.scale(wk,(80,80))
+wk_small=pygame.image.scale(wk,(65,65))
+#Rook
+wr= pygame.image.load('/assets/images/wR.png')
+wr=pygame.image.scale(wr,(80,80))
+wr_small=pygame.image.scale(wr,(65,65))
+#Knight
+wn= pygame.image.load('/assets/images/wN.png')
+wn=pygame.image.scale(wn,(80,80))
+wn_small=pygame.image.scale(wn,(65,65))
+#Pawn
+wp= pygame.image.load('/assets/images/wP.png')
+wp=pygame.image.scale(wp,(60,60))
+wp_small=pygame.image.scale(wp,(45,45))
+#Bishop
+wb= pygame.image.load('/assets/images/wB.png')
+wb=pygame.image.scale(wb,(80,80))
+wb_small=pygame.image.scale(wb,(65,65))
 
 
 #main game loop
