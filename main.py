829c5fce1920e9c7c5b0e20505b1d527935a03ae
Debosmita Bedajna <bedajnadebosmita@gmail.com>
2024-06-25 13:57:33 +0530

    Created the Board

--- main.py
+++ main.py
@@ -3,10 +3,23 @@ import pygame
 pygame.init()
 WIDTH= 1050
 HEIGHT=1050
-screen= pygame.display.seet_mode(WIDTH, HEIGHT)
+screen= pygame.display.set_mode([WIDTH, HEIGHT])
 font= pygame.font.Font('freesansbold.ttf', 20)
 big_font= pygame.font.Font('freesansbold.ttf', 50)
 timer= pygame.time.Clock()
 fps= 60
 
 #game variables 
+
+#main game loop
+run= True
+while run:
+    timer.tick(fps)
+    screen.fill('black')
+    #event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run= False
+        
+    pygame.display.flip()
+pygame.quit()
